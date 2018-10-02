e197e3661478571029a61074d1bf3446dcada920
Kaazikin <eatmybubbles8400@gmail.com>
2018-10-02 16:46:54 -0400

    Update responses.js

--- responses.js
+++ responses.js
@@ -1,4 +1,4 @@
-//responses   
+//responses 
              if(message.content.startsWith('hello')){
                message.channel.send(':wave:')
               message.channel.send(`hey ${message.author.username}`)
@@ -6,4 +6,8 @@
 if(message.content === "kms") {
    const ayy = client.emojis.find("name", "thinking");
    message.reply(`${thinking} LMAO`);
+}
+
+if(message.content == "kaaz"{
+   message.channel.send(`ikin`)
 }
